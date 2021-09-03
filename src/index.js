1bb8f2f7671ec9de6ed1515dcde443db6e80be49
BaherWY <baher.wael@gmail.com>
2021-09-03 10:52:34 +0200

    display board

--- src/index.js
+++ src/index.js
@@ -2,27 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class ShoppingList extends React.Component {
-    render() {
-        return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        );
-    }
-}
+// class ShoppingList extends React.Component {
+//     render() {
+//         return (
+//             <div className="shopping-list">
+//                 <h1>Shopping List for {this.props.name}</h1>
+//                 <ul>
+//                     <li>Instagram</li>
+//                     <li>WhatsApp</li>
+//                     <li>Oculus</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value
+        };
+    }
+
     render() {
         return (
-            <button className="square"
-                    onClick={function() { console.log('click'); }}>
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
