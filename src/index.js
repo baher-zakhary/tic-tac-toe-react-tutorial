f10a9d24c9f4f4a728cdbfb82605119565fe0d9c
BaherWY <baher.wael@gmail.com>
2021-09-04 14:25:28 +0200

    Turning Square component into a controlled component

--- src/index.js
+++ src/index.js
@@ -18,35 +18,50 @@ import './index.css';
 // }
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value
-        };
-    }
 
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+                onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
+    }
+
+    onClickSquare() {
+
     }
 }
 
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
 
         return (
             <div>
-                <div className="status">{status}</div>
+                <div className="status">Next player:</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
